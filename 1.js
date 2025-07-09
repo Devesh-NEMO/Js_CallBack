b9f96368c36468b299361f978713d73b262244b8
Devesh <nemopersonal.d@gmail.com>
2025-07-10 00:11:22 +0530

    Update: added new feature or fixed bug

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 console.log("start")
 
 setTimeout(function cb (){
-    console.log("Callback");
+    console.log("Callback 'output after 5 seconds' ");
 }, 5000 );
 
 console.log("end");
@@ -13,5 +13,27 @@ function hello(x){
 
 hello(function x() {
     console.log("hello x");
-});
+}, 10000);
 
+
+
+const arr = [5,4,9,2,8]
+
+function double(x){
+    return x * 2;
+}
+
+function triple(x){
+    return x * 3;
+}
+function binary(x){
+    return x.toString(2)
+}
+
+const output = arr.map(double);
+const output1 = arr.map(triple);
+const output2 = arr.map(binary);
+
+console.log(output)
+console.log(output1)
+console.log(output2)
